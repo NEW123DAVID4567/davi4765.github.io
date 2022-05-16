860a41b646550281b081cc10d1bea366da5c7329
davi4765 <97152760+NEW123DAVID4567@users.noreply.github.com>
2022-05-16 16:20:51 +0200

    Add files via upload

--- davi4765.github.io-maineieie/script.js
+++ davi4765.github.io-maineieie/script.js
@@ -13,14 +13,14 @@ function myFunction() {
 var acc = document.getElementsByClassName("accordion");
 var i;
 
-let myName = "David";
+let myName = "Om mig";
 let firstlink = document
 .getElementsByTagName("a")[0];
 firstlink.innerHTML = myName;
  
 let title = document
 .getElementsByTagName("title")[0];
-title.innerHTML = myName + "s Logbog";
+title.innerHTML = "Davids Logbog";
 
 for (i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function() {
